c3c41464ac2dd1628857a3b861bebf5611c953e2
RaheelFarouk <raheelfarouk@gmail.com>
2022-10-11 09:24:12 -0400

    Initial Commit

---
+++ HelloWorld/onelight/onelight.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(6, OUTPUT);
+  pinMode(9, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(9)==HIGH){
+    digitalWrite(6,HIGH);
+    delay(500);
+    digitalWrite(6,LOW);
+    delay(500);
+    
+    
+  }else{
+    digitalWrite(6, LOW);
+  }
+  
+  
+
+}
---
+++ HelloWorld/threelight/threelight.ino
@@ -0,0 +1,35 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(9)==HIGH){
+    digitalWrite(6,HIGH);
+    delay(500);
+    digitalWrite(6,LOW);
+    delay(500);
+    
+    digitalWrite(7,HIGH);
+    delay(500);
+    digitalWrite(7,LOW);
+    delay(500);
+    
+    digitalWrite(8,HIGH);
+    delay(500);
+    digitalWrite(8,LOW);
+    delay(500);
+    
+  }else{
+    digitalWrite(6, LOW);
+    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
+  }
+  
+  
+
+}
